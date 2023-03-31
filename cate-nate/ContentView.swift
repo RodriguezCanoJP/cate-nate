faf087f3757b1dfab76b225cd1d2401c995dc2d0
Juan Pablo Rodriguez <jp99@estudiantec.cr>
2023-03-31 09:02:33 -0600

    Initial Commit

---
+++ cate-nate/ContentView.swift
@@ -0,0 +1,48 @@
+//
+//  ContentView.swift
+//  cate-nate
+//
+//  Created by Juan Pablo Rodriguez on 29/3/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var textInput: String = ""
+    @State private var textInput2: String = ""
+    @State private var result: String = ""
+    var body: some View {
+        VStack {
+            Text("Cate-Nate")
+                .font(.title)
+            Text("Escriba la primera frase: ")
+                .bold()
+            TextField("String 1", text: $textInput)
+                .padding()
+                .textInputAutocapitalization(.never)
+                .disableAutocorrection(true)
+            Text("Escriba la segunda frase: ")
+                .bold()
+            TextField("String 2", text: $textInput2)
+                .padding()
+                .textInputAutocapitalization(.never)
+                .disableAutocorrection(true)
+            Button {
+                result += textInput + " " + textInput2
+            } label: {
+                Text("Unir")
+            }
+            Text(result)
+                .foregroundColor(.green)
+
+        }
+        .textFieldStyle(.roundedBorder)
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ cate-nate/cate_nateApp.swift
@@ -0,0 +1,17 @@
+//
+//  cate_nateApp.swift
+//  cate-nate
+//
+//  Created by Juan Pablo Rodriguez on 29/3/23.
+//
+
+import SwiftUI
+
+@main
+struct cate_nateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
